bb4fa5f844d75ff436fb39a5f18134778385963e
dymodyy <dilamaudy12@gmail.com>
2023-12-28 03:48:05 -0600

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,38 +2,26 @@ import streamlit as st
 import replicate
 import os
 
-# Fungsi untuk memuat token dari file atau variabel konfigurasi
-def load_replicate_api_token():
-    if 'REPLICATE_API_TOKEN' in st.secrets:
-        return st.secrets['REPLICATE_API_TOKEN']
-    else:
-        return None
-
-# Memuat Replicate API token
-replicate_api = load_replicate_api_token()
-
 # App title
 st.set_page_config(page_title="NutriVeggie Chatbot 🤖")
 
 # Replicate Credentials
 with st.sidebar:
     st.title('NutriVeggie Chatbot 🤖')
-    if replicate_api:
-        st.success('')
+    if 'REPLICATE_API_TOKEN' in st.secrets:
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
     else:
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
             st.warning('')
         else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
-            # Menyimpan token di secrets
-            st.secrets['REPLICATE_API_TOKEN'] = replicate_api
-            # Menyimpan token di environment variable
-            os.environ['REPLICATE_API_TOKEN'] = replicate_api
+            st.success('')
+    os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
     st.subheader('Parameters')
     temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
-    max_length = st.sidebar.slider('max_length', min_value=0, max_value=500, value=250, step=8)
+    max_length = st.sidebar.slider('max_length', min_value=0, max_value=5000, value=2500, step=8)
 
 # Store LLM generated responses
 if "messages" not in st.session_state.keys():
@@ -48,7 +36,7 @@ def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "Halo! Selamat datang di NutriVeggie Chatbot 🤖. Silakan berikan pertanyaan seputar sayuran, nutrisi, atau dunia vegetarian 🥦🥕🍅"}]
 st.sidebar.button('Hapus Pencarian', on_click=clear_chat_history)
 
-# Function for generating LLaMA2 response.
+# Function for generating LLaMA2 response. Refactored from https://github.com/a16z-infra/llama2-chatbot
 def generate_llama2_response(prompt_input):
     string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
     for dict_message in st.session_state.messages:
@@ -67,7 +55,7 @@ if prompt := st.chat_input(disabled=not replicate_api):
     with st.chat_message("user"):
         st.write(prompt)
 
-# Generate a new response if the last message is not from the assistant
+# Generate a new response if last message is not from assistant
 if st.session_state.messages[-1]["role"] != "assistant":
     with st.chat_message("assistant"):
         with st.spinner("Loading..."):

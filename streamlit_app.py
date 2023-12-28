cfd68ac2a75822a77e35d97f4d369b254b12e4ab
dymodyy <dilamaudy12@gmail.com>
2023-12-28 04:17:09 -0600

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,13 +9,14 @@ st.set_page_config(page_title="NutriVeggie Chatbot 🤖")
 with st.sidebar:
     st.title('NutriVeggie Chatbot 🤖')
     if 'REPLICATE_API_TOKEN' in st.secrets:
+        st.success('API key already provided!', icon='✅')
         replicate_api = st.secrets['REPLICATE_API_TOKEN']
     else:
         replicate_api = st.text_input('Enter Replicate API token:', type='password')
         if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
-            st.warning('')
+            st.warning('Please enter your credentials!', icon='⚠️')
         else:
-            st.success('')
+            st.success('Proceed to entering your prompt message!', icon='👉')
     os.environ['REPLICATE_API_TOKEN'] = replicate_api
 
     st.subheader('Parameters')
